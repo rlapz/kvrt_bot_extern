484015e46e77325ca184afe2171d6282093f1b76
Arthur Lapz <arthurlapz@gmail.com>
2025-07-27 18:29:24 +0700

    python: modify args
    Signed-off-by: Arthur Lapz <arthurlapz@gmail.com>

--- python/test.py
+++ python/test.py
@@ -7,9 +7,9 @@ import subprocess
 
 
 CMD_NAME = sys.argv[1]
-CHAT_ID = sys.argv[3]
-USER_ID = sys.argv[4]
-MSG_ID = sys.argv[5]
+CHAT_ID = sys.argv[4]
+USER_ID = sys.argv[5]
+MSG_ID = sys.argv[6]
 
 API = os.getenv('TG_API')
 CFG = os.getenv('TG_CONFIG_FILE')
@@ -31,7 +31,9 @@ proc = [
 ]
 
 try:
-    subprocess.run(proc)
+    print("args: " + str(sys.argv))
+    ret = subprocess.run(proc, stdout=subprocess.PIPE, text=True)
+    print(ret.stdout)
 except Exception as e:
     print(f'{CMD_NAME}: error: {e}')
     exit(1)
